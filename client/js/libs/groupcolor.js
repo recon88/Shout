e8aefcf4a2635ef77fcb9e2df90441b88592f952
AmShaegar <amshaegar@wrenchbox.de>
2015-03-05 21:46:49 +0100

    added channel color

--- client/js/libs/groupcolor.js
+++ client/js/libs/groupcolor.js
@@ -3,7 +3,7 @@ jQuery.getJSON('groups.json').done(function(data) {
   groups_cache = data;
 });
 
-function groupcolor(nick) { 
-  var group = groups_cache.groups[nick];
+function groupcolor(nick) {
+  var group = nick.match(/^#/) ? 'channel' : groups_cache.groups[nick];
   return groups_cache.colors[group] || 'black';
 }
